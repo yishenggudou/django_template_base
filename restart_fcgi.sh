94ee79137a30335c167d10f57fa622c7d3ebfe70
Dell <timger@ubuntu.(none)>
2012-05-04 16:31:51 +0800

    chenge python vision

--- restart_fcgi.sh
+++ restart_fcgi.sh
@@ -5,12 +5,13 @@
 PROJDIR=`pwd`
 PIDFILE="$PROJDIR/www.pid"
 SOCKET="$PROJDIR/www.sock"
+PORT="7171"
 echo ${PROJDIR}
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
 	kill -9 `cat  $PIDFILE`
 	rm -f -- $PIDFILE
 fi
-#cd $PROJDIR && /usr/local/python2.7/bin/python2.7 manage.py runfcgi method=threaded host=127.0.0.1 port=8016 pidfile=$PIDFILE outlog=rout.log errlog=rerr.log
-cd $PROJDIR && sudo /usr/local/python2.7/bin/python2.7 manage.py runfcgi method=threaded host=127.0.0.1 port=7171 pidfile=$PIDFILE outlog="${PROJDIR}rout.log" errlog="${PROJDIR}err.log"
+#cd $PROJDIR && sudo /usr/local/python2.7/bin/python2.7 manage.py runfcgi method=threaded host=127.0.0.1 port=${PORT} pidfile=${PIDFILE} outlog="${PROJDIR}rout.log" errlog="${PROJDIR}err.log"
+cd $PROJDIR && sudo python manage.py runfcgi method=threaded host=127.0.0.1 port=${POR} pidfile=${PIDFILE} outlog="${PROJDIR}rout.log" errlog="${PROJDIR}err.log"
 
